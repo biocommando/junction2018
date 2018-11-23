52c2e2f8dc1204fdb857994428b29ac9fa4dfd9a
Joonas Salonpää <joonas1000@gmail.com>
2018-11-23 20:49:18 +0200

    Add package.json and make basic test that downloads some data from ensto endpoint

--- server.js
+++ server.js
@@ -2,7 +2,32 @@ const express = require('express');
 const app = express();
 const https = require('https');
 const port = 3000;
+const userAndPassword = process.env.JUNCTION2018CREDENTIALS;
+const options = {
+    host: 'junctionev.enstoflow.com',
+    //port: 443,
+    path: '/api/v1/chargingPointGroup',
+    // authentication headers
+    headers: {
+       'Authorization': 'Basic ' + Buffer.from(userAndPassword).toString('base64')
+    }   
+ };
+https.get(options, (resp) => {
+  let data = '';
 
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
